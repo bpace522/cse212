e239e2d18acec606ee8cf3876f92a1810457c657
bpace522 <pacebc522@gmail.com>
2026-01-12 23:51:42 -0700

    learning activities

---
+++ test.cs
@@ -0,0 +1,16 @@
+public static class Examples
+{
+    public static void ExampleArray()
+    {
+        int[] fixedArray = new int[4];
+
+        fixedArray[0] = 1325;
+        fixedArray[1] = 235;
+        fixedArray[2] = 24;
+        fixedArray[3] = 234354;
+
+        List<string> Names = new List<string>();
+
+        Names.Add("This is where I add names");
+    }
+}
